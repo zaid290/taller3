bc3dc41332c2a07be8c3b4f4482a1b0395a6d062
zaid290 <zaid.chamorro@udla.edu.ec>
2025-11-04 21:11:15 -0500

    funciones.h

---
+++ funciones.h
@@ -0,0 +1,6 @@
+int ingresarProductos(char nombres[10][50], float precios[10], int cantidades[10], int n);
+float calcularTotal(float precios[10], int cantidades[10], int n);
+int indiceMasCaro(float precios[10], int n);
+int indiceMasBarato(float precios[10], int n);
+float calcularPromedio(float precios[10], int n);
+int buscarProducto(char nombres[10][50], float precios[10], int cantidades[10], int n, char nombreBuscado[50]);
